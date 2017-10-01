2374f67b3d7930909372aa5de4b3447cbb631ad5
mikadokhaki <athan.d.tolentino@gmail.com>
2017-10-01 09:31:14 -0700

    First commit

---
+++ EclipseGitUpload/src/first/upload/FirstUpload.java
@@ -0,0 +1,13 @@
+package first.upload;
+
+public class FirstUpload {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is my first upload.");
+		System.out.println("Connecting git and selenium.");
+		
+	}
+
+}
